edba29ce340954b30cef4eff634f08a3db542a1e
Alvaro Neira <alvaroneira98@gmail.com>
2025-08-02 12:05:46 -0500

    formgroup and formcontrolname

--- form-angular/src/app/app.ts
+++ form-angular/src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('y');
+  userForm = new FormGroup({
+    username: new FormControl('', [Validators.required] ),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password : new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    role: new FormControl('', [Validators.required])
+  });
+
+  getFormErrors(){
+    const errors: any = []
+    Object.keys(this.userForm.controls).forEach(key => {
+      const control  = this.userForm.get(key);
+      if(control && control.errors){
+        errors[key] = control.errors;
+      }
+    });
+    return errors;
+  }
+
+  onSubmit(){
+    console.log('Form Submitted', this.userForm.value);
+  }
 }
