8d81a3c4e5b169644a763c6fedda84f9f2d40cce
Alvaro Neira <alvaroneira98@gmail.com>
2025-08-07 18:39:30 -0500

    dynamic-fomr

---
+++ dynamic-form-angular/src/app/app.ts
@@ -0,0 +1,153 @@
+import { Subscription } from 'rxjs';
+
+import { Component, computed, inject, signal, OnDestroy } from '@angular/core';
+
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+
+
+// Interface para producto
+interface Product {
+  id: number;
+  name: string;
+  priceUnitario: number;
+}
+
+// Tipo de producto para el formulario
+type ProductForm = FormGroup<{
+  product: FormControl<Product | null>;
+  subTotal: FormControl<number>;
+  quantity: FormControl<number>;
+}>;
+
+// Tipo de formulario principal
+type Form = FormGroup<{
+  client: FormControl<string>;
+  products: FormArray<ProductForm>;
+  Total: FormControl<number>;
+}>;
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+
+  private subscription = new Subscription();
+
+  // Array de productos para el formulario
+  productos: Product[] = [
+    { id: 1, name: 'Cucumber', priceUnitario: 15.5 },
+    { id: 2, name: 'Tomato', priceUnitario: 10 },
+    { id: 3, name: 'Carrot', priceUnitario: 12 },
+    { id: 4, name: 'Apple', priceUnitario: 11 },
+  ];
+
+  // Referencia para acceder al array de productos del formulario
+  get productArray(): FormArray<ProductForm> {
+    return this.form.get('products') as FormArray<ProductForm>;
+  }
+
+  get Total(): Number {
+    return this.form.get('Total')?.value || 0;
+  }
+
+  // Injectar NonNullableFormBuilder para evitar valores nulos
+  formBuilder = inject(NonNullableFormBuilder);
+
+  form: Form = this.formBuilder.group({
+    client: [''],
+    products: this.formBuilder.array<ProductForm>([this.createProductForm()]),
+    Total: [0],
+  });
+
+
+  constructor() {
+    this.subscription.add(
+      this.productArray.valueChanges.subscribe(() => {
+        console.log('Productos cambiados');
+        console.log(this.productArray.controls[0].value.product?.priceUnitario);
+        this.updateTotal();
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  // ✅ Método para calcular y actualizar el total
+  private updateTotal(): void {
+    const total = this.productArray.controls.reduce((sum, control) => {
+      const subTotal = control.get('subTotal')?.value || 0;
+      return sum + subTotal;
+    }, 0);
+    this.form.get('Total')?.setValue(total);
+  }
+
+
+
+
+  createProductForm(): ProductForm {
+    return this.formBuilder.group({
+      product: this.formBuilder.control<Product | null>(null),
+      subTotal: this.formBuilder.control<number>(0),
+      quantity: this.formBuilder.control<number>(0),
+    });
+  }
+
+  save() {
+    console.log('save');
+    console.log(this.form.value);
+  }
+
+  change(e: MatSelectChange, index: number) {
+    this.productArray.at(index).patchValue({
+      subTotal: e.value?.priceUnitario || 0, // Actualizar precio basado en el producto seleccionado
+      quantity: 1, // Reiniciar cantidad al seleccionar un nuevo producto
+    });
+
+  }
+
+
+  changeQuantity(index: number, e: Event) {
+    if (!this.productArray.at(index)) return;
+    if (index < 0) return;
+    const quantity = Number((e.target as HTMLInputElement).value);
+
+    const productForm = this.productArray.at(index);
+    const price = productForm.get('product')?.value?.priceUnitario || 0;
+
+    productForm.patchValue({
+      quantity,
+      subTotal: price * quantity
+    });
+  }
+
+  add() {
+    console.log('add');
+    this.productArray.push(this.createProductForm());
+  }
+
+  delete(index: number) {
+    console.log('delete', index);
+    this.productArray.removeAt(index);
+  }
+}
