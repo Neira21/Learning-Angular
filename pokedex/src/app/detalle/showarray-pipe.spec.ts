d213f2b97d6075c75a173135aaa28605a1a399a5
Alvaro Neira <alvaroneira98@gmail.com>
2025-08-12 14:33:30 -0500

    readme update

--- pokedex/src/app/detalle/showarray-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowarrayPipe } from './showarray-pipe';
-
-describe('ShowarrayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShowarrayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
