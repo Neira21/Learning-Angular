c9e74cd3ad15c478c2f0b5f7630170b5be31fbe6
Alvaro Neira <alvaroneira98@gmail.com>
2025-10-30 17:51:57 -0500

    i18n select language

--- i18n-internacionalizacion/src/app/components/header/header.ts
+++ i18n-internacionalizacion/src/app/components/header/header.ts
@@ -1,4 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, LOCALE_ID, signal, computed } from '@angular/core';
+
+interface Language {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -9,4 +14,28 @@ import { Component, signal } from '@angular/core';
 export class Header {
   title = signal($localize`Ejemplo de Internacionalización`);
   subtitle = signal($localize`Aprendiendo a usar i18n, traducir textos y más`);
+
+  private readonly locale = inject(LOCALE_ID);
+
+  languages = signal<Language[]>([
+    { name: 'Español - Perú', code: 'es-PE' },
+    { name: 'Inglés', code: 'en' },
+    { name: 'Italiano', code: 'it' },
+    { name: 'Francés', code: 'fr' },
+    { name: 'Alemán', code: 'de' },
+  ]);
+
+  currentLanguage = computed(
+    () =>
+      this.languages().find((lang) => lang.code === this.locale)?.code ||
+      'es-PE'
+  );
+
+  changeValue(newLanguage: string): void {
+    if (this.currentLanguage() === newLanguage) return;
+
+    const baseurl = window.location.origin;
+    const newUrl = `${baseurl}/${newLanguage}`;
+    window.location.href = newUrl;
+  }
 }
